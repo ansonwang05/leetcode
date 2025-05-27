a04fbf25dd8bf154056fa8b14ed29d2cc172a804
Anson Wang <156279173+ansonwang05@users.noreply.github.com>
2025-05-27 14:53:29 -0400

    Update 242. Valid Anagram.py

--- array+hashing/242. Valid Anagram.py
+++ array+hashing/242. Valid Anagram.py
@@ -1,1 +1,22 @@
-
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        seen = {}
+        for letter in s: 
+            if letter not in seen: 
+                seen[letter] = 1
+            else:
+                seen[letter] += 1
+        for letter in t:
+            if letter in seen: 
+                seen[letter] -= 1
+            else:
+                return False
+        for key, value in seen.items():
+            if value != 0:
+                return False
+        return True
