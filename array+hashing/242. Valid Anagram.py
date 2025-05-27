b2bd3b1fb35dee3ce936e480a96d7606ed55c2a3
Anson Wang <156279173+ansonwang05@users.noreply.github.com>
2025-05-27 15:15:53 -0400

    Update 242. Valid Anagram.py

--- array+hashing/242. Valid Anagram.py
+++ array+hashing/242. Valid Anagram.py
@@ -20,3 +20,32 @@ class Solution(object):
             if value != 0:
                 return False
         return True
+'''
+Instead of running two for loops you can just run one for loop and check if the letter is in the the string s and the string t
+An extra condition of this is that you have to check if the strings are now the same length, else you can iterate both strings the same time. 
+'''
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        seen = {}
+        if len(s) != len(t): 
+            return False
+        for index in range(len(s)): 
+            sLetter = s[index]
+            tLetter = t[index] 
+            if sLetter not in seen:
+                seen[sLetter] = 1
+            else: 
+                seen[sLetter] += 1
+            if tLetter not in seen: 
+                seen[tLetter] = -1 
+            else: 
+                seen[tLetter] -= 1
+        for key, value in seen.items():
+            if value != 0: 
+                return False
+        return True
