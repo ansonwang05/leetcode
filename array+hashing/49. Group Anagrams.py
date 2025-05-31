a3adf911b6a7975079c73618b8c9d91cf9d2d75d
Anson Wang <156279173+ansonwang05@users.noreply.github.com>
2025-05-31 16:53:08 -0400

    Update 49. Group Anagrams.py

--- array+hashing/49. Group Anagrams.py
+++ array+hashing/49. Group Anagrams.py
@@ -1,1 +1,21 @@
-
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        result = []
+        seen = {}
+        for word in strs: 
+            arrKey = [0] * 26
+            for letter in word: 
+                index = ord(letter) - ord("a")
+                arrKey[index] += 1
+            tupKey = tuple(arrKey)
+            if tupKey not in seen:
+                seen[tupKey] = [word]
+            else: 
+                seen[tupKey].append(word)
+        for key, value in seen.items(): 
+            result.append(value)
+        return result
