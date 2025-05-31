99a1d985be29d45ed9bef55c48559fd31b766149
Anson Wang <156279173+ansonwang05@users.noreply.github.com>
2025-05-31 16:58:22 -0400

    Update 49. Group Anagrams.py

--- array+hashing/49. Group Anagrams.py
+++ array+hashing/49. Group Anagrams.py
@@ -1,3 +1,12 @@
+'''
+In this problem we use a dictionary that has tuple as keys and an array as values
+The keys are tuples because python dictionary requires keys to be immutable and arrays are mutable
+The keys are tuples that are converted from array which keeps track of the ammount of letters in a word
+    If a word has the same amount of letters as another word then it is a anagram otherwise it is not
+The values are arrays that keep track of each word that is an anagram of another word
+At the end we iterate through the dictionary to return all the arrays of anagrams appened to result
+'''
+
 class Solution(object):
     def groupAnagrams(self, strs):
         """
