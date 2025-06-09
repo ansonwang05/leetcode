65ef10de59b97f2ad91fa12d9fc73bb4f7947c5f
Anson Wang <156279173+ansonwang05@users.noreply.github.com>
2025-06-09 17:25:05 -0400

    Update 347. Top K Frequent Elements.py

--- array+hashing/347. Top K Frequent Elements.py
+++ array+hashing/347. Top K Frequent Elements.py
@@ -1,1 +1,21 @@
-
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        freq = {}
+        result = []
+        for num in nums: 
+            if num not in freq: 
+                freq[num] = 1
+            else:
+                freq[num] += 1
+        freqList = sorted(list(freq.items()), key=lambda x: x[1], reverse=True)
+        if len(freqList) == 1: 
+            result.append(freqList[0][0])
+            return result
+        for i in range(k): 
+            result.append(freqList[i][0])
+        return result
