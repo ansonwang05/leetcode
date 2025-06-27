f905166ec382f5fa73a24cecfe20831688ca2be1
Anson Wang <156279173+ansonwang05@users.noreply.github.com>
2025-06-27 18:10:19 -0400

    Update 128. Longest Consecutive Sequence.py

--- array+hashing/128. Longest Consecutive Sequence.py
+++ array+hashing/128. Longest Consecutive Sequence.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-# First Try using sorting which is O(n log n) not very efficient
+# First try using sorting which is O(n log n) not very efficient
         if len(nums) == 0: 
             return 0
 
@@ -23,3 +23,15 @@ class Solution(object):
                 currSeq = 1
         consec.append(currSeq)
         return max(consec)
+# Second try using optimal solution which is O(n) and space of O(n)
+        setNums = set(nums) 
+        longest = 0 
+        
+        for num in setNums: 
+            prev = num - 1
+            if (num - 1) not in setNums: 
+                length = 1
+                while (num + length) in setNums: 
+                    length += 1 
+                longest = max(length, longest) 
+        return longest
