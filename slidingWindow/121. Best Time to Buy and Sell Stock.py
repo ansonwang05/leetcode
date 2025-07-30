1812308e6caa704293e0cbed6fafde07b26a5534
Anson Wang <156279173+ansonwang05@users.noreply.github.com>
2025-07-30 19:03:30 -0400

    Update 121. Best Time to Buy and Sell Stock.py

--- slidingWindow/121. Best Time to Buy and Sell Stock.py
+++ slidingWindow/121. Best Time to Buy and Sell Stock.py
@@ -1,1 +1,18 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = highest = 0 
+        lowest = prices[0] 
+        for price in prices: 
+            if price < lowest: 
+                lowest = price 
+                highest = 0 
+            elif price > highest: 
+                highest = price 
+                profit = max(profit, highest - lowest)
+
+        return profit
 
