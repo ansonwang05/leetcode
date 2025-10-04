d5b91727d83eb7bf8f671e77e2833a4a6f2b2be8
Anson <156279173+ansonwang05@users.noreply.github.com>
2025-10-04 15:02:24 -0400

    Add O(1) Solution using math

--- daily/3100. Water Bottles II.py
+++ daily/3100. Water Bottles II.py
@@ -1,4 +1,5 @@
 class Solution(object):
+    # Solution is O(sqrt(n)), there is a way to optimize 
     def maxBottlesDrunk(self, numBottles, numExchange):
         """
         :type numBottles: int
@@ -13,3 +14,18 @@ class Solution(object):
             empty += 1
             numExchange += 1
         return total + full
+    
+    # Optimize solution is O(1), using math
+    def maxBottlesDrunk(self, numBottles, numExchange):
+        """
+        :type numBottles: int 
+        :type numExchange: int
+        :rtype: int
+        """
+        a = 1
+        b = 2 * numExchange - 3
+        c = -2 * numBottles
+        delta = b * b - 4 * a * c
+        t = int(math.ceil((-b + math.sqrt(delta))) / (2 * a))
+        return numBottles
+
