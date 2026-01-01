dc04a8c681f09b18dd00830bc3d783c0fed51a0f
Anson <156279173+ansonwang05@users.noreply.github.com>
2026-01-01 01:45:26 -0500

    Solved Question 153
    Binary Search question in O(log n)

---
+++ binarySearch/153. Find Minimum in Rotated Sorted Array.py
@@ -0,0 +1,18 @@
+# Solution needs to be in O(log n) hence Binary Search
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = 0 
+        right = len(nums) - 1 
+        while left < right: 
+            mid = (left + right) // 2
+
+            if nums[mid] < nums[right]: 
+                right = mid 
+            else: 
+                left = mid + 1 
+
+        return nums[left]
